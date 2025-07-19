23c6e73e8954e470a06edd0dc6e0c7d40613e4c7
ayshenmehdiyev <aysnmehdiyeva521@gmail.com>
2025-07-19 22:55:54 +0400

    Added localStorage

--- Modul-02/UserFormApp/main.js
+++ Modul-02/UserFormApp/main.js
@@ -3,11 +3,19 @@ let useremailInput = document.getElementById("useremail");
 let passwordInput = document.getElementById("password");
 let addbtn = document.getElementById("addbtn");
 let changebtn = document.getElementById("changebtn");
+let search = document.getElementById("search");
+let getinfo = document.getElementById("getinfo");
 let tbody = document.querySelector("tbody");
 
 
-let users = [];
-let id = 0;
+let localeUsers = JSON.parse(localStorage.getItem("users"))
+let localID = localStorage.getItem("usersId")
+render(localeUsers)
+
+
+
+let users = [...localeUsers];
+let id = localID;
 let globalId = 0;
 
 addbtn.addEventListener("click", function (e) {
@@ -18,10 +26,12 @@ addbtn.addEventListener("click", function (e) {
         useremail: useremail.value,
         password: passwordInput.value
     });
-    render()
+    render(users)
     fullnameInput.value = "";
     useremail.value = "";
     passwordInput.value = "";
+    localStorage.setItem("users", JSON.stringify(users))
+    localStorage.setItem("usersId", (id))
 
 })
 
@@ -30,13 +40,16 @@ changebtn.addEventListener("click", function () {
     addbtn.classList.remove("d-none");
     changebtn.classList.add("d-none");
     users.forEach(user => {
-        if (user.id === globalId){
+        if (user.id === globalId) {
             user.fullname = fullnameInput.value;
             user.useremail = useremailInput.value;
             user.password = passwordInput.value;
         }
     })
-    render()
+    render(users)
+    fullnameInput.value = "";
+    useremailInput.value = "";
+    passwordInput.value = "";
 })
 
 
@@ -46,21 +59,33 @@ function Update(param) {
     addbtn.classList.add("d-none");
     changebtn.classList.remove("d-none");
     const { fullname, useremail, password } = users.find(user => user.id === param);
-        fullnameInput.value = fullname
-        useremailInput.value = useremail
-        passwordInput.value = password
+    fullnameInput.value = fullname
+    useremailInput.value = useremail
+    passwordInput.value = password
 
 }
 
 function Delete(param) {
     users = users.filter(user => user.id !== param)
-    render()
+    render(users)
 }
 
-function render() {
+
+
+search.addEventListener("keyup", function (e) {
+    let filterusers = users.filter(user =>
+        user.fullname.toLowerCase().includes(search.value.toLowerCase())
+    );
+    render(filterusers)
+
+})
+
+
+function render(param) {
     tbody.innerHTML = "";
-    users.forEach(user => {
-        tbody.innerHTML += `<tr>
+    param.forEach(user => {
+        if (user.fullname !== "" && user.useremail !== "") {
+            tbody.innerHTML += `<tr>
                 <td scope="row">${user.id}</td>
                 <td>${user.fullname}</td>
                 <td>${user.useremail}</td>
@@ -68,7 +93,8 @@ function render() {
                     <button class="btn btn-danger" onclick="Delete(${user.id})" >delete</button>
                     <button class="btn btn-secondary" onclick="Update(${user.id})">update</button>
                 </td>
-            </tr>`
+            </tr>`}
+
     })
 }
 
